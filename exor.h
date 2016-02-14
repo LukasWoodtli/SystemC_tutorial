f98ec8ee0e65648638f17c4f4292fd24af49e60c
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-02-15 00:06:26 +0100

    Initial commit
    Beginning of basic SystemC tutorial

---
+++ exor.h
@@ -0,0 +1,28 @@
+#include <systemc.h>
+
+#include "nand.h"
+
+SC_MODULE(exor) {
+  sc_in<bool> A, B;
+  sc_out<bool> F;
+
+  nand n1, n2, n3, n4;
+
+  sc_signal<bool> S1, S2, S3;
+
+  SC_CTOR(exor) : n1("N1"), n2("N2"), n3("N3"), n4("N4") {
+    n1.A(A);
+    n1.B(B);
+    n1.F(S1);
+
+    n2 << A << S1 << S2;
+
+    n3(S1);
+    n3(B);
+    n3(S3);
+
+    n4 << S2 << S3 << F;
+  }
+};
+
+
---
+++ nand.h
@@ -0,0 +1,21 @@
+#ifndef _NAND_H__
+#define _NAND_H__
+
+#include <systemc.h>
+
+SC_MODULE(nand) {
+  sc_in<bool> A, B;
+  sc_out<bool> F;
+
+  void do_it() {
+    F.write(!(A.read() && B.read()));
+  }
+
+  SC_CTOR(nand) {
+    SC_METHOD(do_it);
+    sensitive << A << B;
+  }
+
+};
+
+#endif /* _NAND_H__ */
