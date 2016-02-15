dedd699b6a80ba940813443a0d2ecea7044f0d4a
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-02-15 22:38:41 +0100

    Remove warning from simulation kernel

--- exor.h
+++ exor.h
@@ -16,19 +16,23 @@ SC_MODULE(exor) {
 
   SC_CTOR(exor) : n1("N1"), n2("N2"), n3("N3"), n4("N4") {
     
-      cout << "Construction of EXOR: " << name() << endl;
+    cout << "Construction of EXOR: " << name() << endl;
     
     n1.A(A);
     n1.B(B);
     n1.F(S1);
 
-    n2 << A << S1 << S2;
+    n2.A(A);
+    n2.B(S1);
+    n2.F(S2);
+      
+    n3.A(S1);
+    n3.B(B);
+    n3.F(S3);
 
-    n3(S1);
-    n3(B);
-    n3(S3);
-
-    n4 << S2 << S3 << F;
+    n4.A(S2);
+    n4.B(S3);
+    n4.F(F);
   }
 };
 
